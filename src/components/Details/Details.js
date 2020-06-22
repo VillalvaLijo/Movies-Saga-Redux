657910b2c2eef7f257b2ac3e57718dbdaa3b1d80
Samuel Villalva Lijo <samuelvillalvalijo@gmail.com>
2020-06-21 19:53:28 -0500

    got genres to display on the detail page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -5,9 +5,32 @@ import {connect} from 'react-redux';
 
 class Details extends Component{
     //access the movie in details by passing the id of the movie on the click.
-
+    //create a local state to hold all the genres of the movie
+    state = {
+        genreArray: []
+    }
     componentDidMount(){
+        //grab all genres from query with equal movie id to the movie passed in.
+        this.selectGenreByMovieId()
+    }
 
+    selectGenreByMovieId(){
+        let genreArray = [];
+        // for( of this.props.reduxState.genres){
+        //     if (this.props.reduxState.details.id === genre_list_item.id)
+        //         genreArray.push(genre_list_item.name);
+        // }
+        //console.log(this.props.reduxState.genres.length)
+        for(let i=0; i<this.props.reduxState.genres.length;i++){
+            if(this.props.reduxState.details.id === this.props.reduxState.genres[i].movie_id){
+                console.log(this.props.reduxState.genres[i].name);
+                genreArray.push(this.props.reduxState.genres[i].name);
+            }
+        }
+        this.setState({
+            genreArray: genreArray,
+        });
+        console.log(genreArray);
     }
 
     render(){
@@ -15,7 +38,8 @@ class Details extends Component{
             <div>
                 <img src ={this.props.reduxState.details.poster}/>
                 <h2>{this.props.reduxState.details.title}</h2>
-                <p>{JSON.stringify(this.props.reduxState.genres)}</p>
+                {/* <p>{JSON.stringify(this.props.reduxState.genres)}</p> */}
+                <h4>{this.state.genreArray}</h4>
                 <p>{this.props.reduxState.details.description}</p>
 
             </div>
